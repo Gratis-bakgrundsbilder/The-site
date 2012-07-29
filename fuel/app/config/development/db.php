0a2e6f98109bfd3005c7531a3c807f8f4b80e335
Sony? <sony@4morefun.net>
2012-07-29 23:35:41 +0200

    Added an example of how to connect to a PDO db.

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -5,12 +5,21 @@
 
 return array(
 	'default' => array(
+        //MySQL and MySQLi connections
+        /*
 		'connection'  => array(
             'hostname'   => 'localhost',
             'port'       => '3306',
             'database'   => 'gratis-bakgrundsbilder',
 			'username'   => 'root',
 			'password'   => '',
-		),
+		),*/
+
+        //PDO connections
+        'connection'  => array(
+            'dsn'        => 'mysql:host=localhost;dbname=gratis-bakgrundsbilder',
+            'username'   => 'root',
+            'password'   => '',
+        ),
 	),
 );
